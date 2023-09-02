36d1d044f4e614033c62e5b2879a452390a87def
kleber lucas <117679545+kleberlucas20014@users.noreply.github.com>
2023-09-02 02:30:00 +0100

    melhorias no app

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
 
-const key = "9ebbccad1023e8c8f213d4c5193c5d77"
+let  key = "9ebbccad1023e8c8f213d4c5193c5d77"
+
 function dataOnScreen(dados){
-    document.querySelector(".city").innerHTML = "Tempo em" + dados.name
-    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°"
+    console.log(dados)
+    document.querySelector(".city").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
     document.querySelector(".description").innerHTML = dados.weather[0].description
-    document.querySelector(".humidity").innerHTML = dados.main.humidity + "%"
-    ducument.querySelector("img-forecast").src = 'https://openweathermap.org/img/wn/${dados.weather[0].icon}.png'
+    ducument.querySelector("icone-cloud").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icone-cloud + ".png"
 
 }
 
  async function searchCity(city){
-    const dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt-pt&units=metric').then(answear => answear.json())
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+key+"&lang=pt-pt"+"&units=metric").then(answear => answear.json())
     
     dataOnScreen(dados)
 
 }
 
 function clickedbutton(){
-    const city = document.querySelector(".input-city").value
+    let city = document.querySelector(".input-city").value
     
 
     searchCity(city)
